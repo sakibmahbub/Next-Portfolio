484fababdb8bb3e0c8d3f960be9a788df245d92f
sakibmahbub <arafatsakib48@gmail.com>
2023-07-01 20:50:44 +0600

    created hero section

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,65 @@
+// next image
+import Image from "next/image";
+
+// components
+import ParticlesContainer from "../components/ParticlesContainer";
+import ProjectsBtn from "../components/ProjectsBtn";
+import Avatar from "../components/Avatar";
+
+// framer motion
+import { motion } from "framer-motion";
+
+// variants
+import { fadeIn } from "../variants";
 
 const Home = () => {
-  return <div>Home</div>;
+  return (
+    <div className=" bg-primary/60 h-full">
+      {/* text */}
+      <div className="w-full h-full bg-gradient-to-r from-primary/10 via-black/30 to-black/10">
+        <div className="text-center flex flex-col justify-center xl:pt-40 xl:text-left h-full container mx-auto">
+          {/* title */}
+          <motion.h1
+            variants={fadeIn("down", 0.2)}
+            initial="hidden"
+            animate="show"
+            exit="hidden"
+            className="h1"
+          >
+            Transforming Ideas <br /> Into{" "}
+            <span className="text-accent">Digital Reality</span>
+          </motion.h1>
+          {/* sub-title */}
+          <motion.p
+            variants={fadeIn("down", 0.3)}
+            initial="hidden"
+            animate="show"
+            exit="hidden"
+            className="max-w-sm xl:max-w-xl mx-auto xl:mx-0 mb-10 xl:mb-16"
+          >
+            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Totam
+            aliquid quis nihil voluptatibus libero assumenda, ipsum quo vero
+            provident minus!
+          </motion.p>
+          {/* btn */}
+          <div className="flex justify-center xl:hidden relative">
+            <ProjectsBtn />
+          </div>
+          <motion.div
+            variants={fadeIn("down", 0.4)}
+            initial="hidden"
+            animate="show"
+            exit="hidden"
+            className="hidden xl:flex"
+          >
+            <ProjectsBtn />
+          </motion.div>
+        </div>
+      </div>
+      {/* image */}
+      <div></div>
+    </div>
+  );
 };
 
 export default Home;
