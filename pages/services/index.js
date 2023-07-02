3c30fbadd9709554a418e482c4945c6a418dc726
sakibmahbub <arafatsakib48@gmail.com>
2023-07-03 02:37:46 +0600

    added services slider

--- pages/services/index.js
+++ pages/services/index.js
@@ -9,9 +9,45 @@ import { fadeIn } from "../../variants";
 
 const Services = () => {
   return (
-    <div className="h-full bg-primary/30 py-36 flex items-center">
+    <div className="h-full bg-primary/30 py-36 flex justify-center items-center">
       <Circles />
-      <div className="container mx-auto"></div>
+      <div className="container mx-auto">
+        <div className="flex flex-col xl:flex-row gap-x-8">
+          {/* text */}
+          <div className="text-center flex xl:w-[30vw] flex-col lg:text-left mb-4 xl:mb-0">
+            <motion.h2
+              variants={fadeIn("up", 0.3)}
+              initial="hidden"
+              animate="show"
+              exit="hidden"
+              className="h2 xl:mt-8"
+            >
+              My Services <span className="text-accent">.</span>
+            </motion.h2>
+            <motion.p
+              variants={fadeIn("up", 0.4)}
+              initial="hidden"
+              animate="show"
+              exit="hidden"
+              className="mb-4 max-w-[400px] mx-auto lg:mx-0"
+            >
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Rerum,
+              suscipit repudiandae magni sapiente autem ipsam tempore error
+              maxime a ex?
+            </motion.p>
+          </div>
+          {/* slider */}
+          <motion.div
+            variants={fadeIn("down", 0.6)}
+            initial="hidden"
+            animate="show"
+            exit="hidden"
+            className="w-full xl:max-w-[65%]"
+          >
+            <ServiceSlider />
+          </motion.div>
+        </div>
+      </div>
       <Bulb />
     </div>
   );
