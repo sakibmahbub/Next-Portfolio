eaa10c49c32d56f876a313df40cc736b1e337fe7
sakibmahbub <arafatsakib48@gmail.com>
2023-07-02 12:46:40 +0600

    added particles and images on hero section

--- pages/index.js
+++ pages/index.js
@@ -7,10 +7,11 @@ import ProjectsBtn from "../components/ProjectsBtn";
 import Avatar from "../components/Avatar";
 
 // framer motion
-import { motion } from "framer-motion";
+import { easeInOut, motion } from "framer-motion";
 
 // variants
 import { fadeIn } from "../variants";
+import Particles from "react-tsparticles";
 
 const Home = () => {
   return (
@@ -57,7 +58,25 @@ const Home = () => {
         </div>
       </div>
       {/* image */}
-      <div></div>
+      <div className="w-[1200px] h-full absolute right-0 bottom-0">
+        {/* bg image */}
+        <div className="bg-none xl:bg-explosion xl:bg-cover xl:bg-right xl:bg-no-repeat w-full h-full absolute mix-blend-color-dodge translate-z-0"></div>
+        {/* particles */}
+        <div>
+          <ParticlesContainer />
+        </div>
+        {/* avatar img */}
+        {/* <motion.div
+          variants={fadeIn("up", 0.5)}
+          initial="hidden"
+          animate="show"
+          exit="hidden"
+          transition={{ duration: 1, ease: easeInOut }}
+          className="w-full h-full max-w-[737px] max-h-[678px] absolute -bottom-32 lg:bottom-0 lg:right-[8%]"
+        >
+          <Avatar />
+        </motion.div> */}
+      </div>
     </div>
   );
 };
