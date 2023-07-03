c28b15b4c11381570ec583e48461c4d163419d08
sakibmahbub <arafatsakib48@gmail.com>
2023-07-03 23:16:59 +0600

    added download resume on header

--- components/Header.js
+++ components/Header.js
@@ -6,6 +6,7 @@ import Link from "next/link";
 
 // components
 import Socials from "../components/Socials";
+import { BsArrowDown } from "react-icons/bs";
 
 const Header = () => {
   return (
@@ -20,6 +21,14 @@ const Header = () => {
           </Link>
           {/* socials */}
           <Socials />
+          <Link href="/Resume.pdf">
+            <button className="btn rounded-full border border-white/50 max-w-[170px] px-8 transition-all duration-300 flex items-center justify-center overflow-hidden hover:border-accent group">
+              <span className="group-hover:-translate-y-[120%] group-hover:opacity-0  transition-all duration-500">
+                Resume
+              </span>
+              <BsArrowDown className="-translate-y-[120%] opacity-0 group-hover:flex group-hover:-translate-y-0 group-hover:opacity-100 transition-all duration-300 absolute text-[22px]" />
+            </button>
+          </Link>
         </div>
       </div>
     </header>
